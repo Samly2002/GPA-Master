c3dfad4f1a655c1d6110949fd8e62ab476e153a4
samly2002 <j.sanonashath@gmail.com>
2025-03-12 20:46:03 +0530

    Initial commit

---
+++ js/course.js
@@ -0,0 +1,346 @@
+
+var courseName = "Infomation Communication Technology- University of Sri Jayewardenepura";
+
+var courseUnits = [
+  {
+    year: "1",
+    sems: [
+      {
+        sem: "1",
+        subs: [
+          {
+            name: "Mathematics I",
+            id: "ITC 1013",
+          },
+          {
+            name: "Physics for Technology",
+            id: "ITC 1023",
+          },
+          {
+            name: "Principles of Statistics",
+            id: "ITC 1032",
+          },
+          {
+            name: "Software Engineering I",
+            id: "ITC 1052",
+          },
+          {
+            name: "Fundamentals of Programming",
+            id: "ITC 1063",
+          },
+          {
+            name: "Communication Skills I/English",
+            id: "ITC 1082",
+          },
+          {
+            name: "Computer Organization and Architecture",
+            id: "ITC 1142",
+          },
+        ],
+      },
+      {
+        sem: "2",
+        subs: [
+          {
+            name: "Mathematics II",
+            id: "ITC 1102",
+          },
+          {
+            name: "Statistical Methods",
+            id: "ITC 1112",
+          },
+          {
+            name: "Object Oriented Analysis and Design",
+            id: "ITC 1153",
+          },
+          {
+            name: "Database Systems",
+            id: "ITC 1163",
+          },
+          {
+            name: "Communication Skills II/English",
+            id: "ITC 1172",
+          },
+          {
+            name: "Object Oriented Programming",
+            id: "ITC 1233",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "2",
+    sems: [
+      {
+        sem: "3",
+        subs: [
+          {
+            name: "Mathematics for ICT",
+            id: "ITC 2192",
+          },
+          {
+            name: "Data Structures and Algorithms",
+            id: "ITC 2212",
+          },
+          {
+            name: "Network Essentials",
+            id: "ITC 2243",
+          },
+          {
+            name: "Operating Systems",
+            id: "ITC 2272",
+          },
+          {
+            name: "Visual Application Programming",
+            id: "ITC 2303",
+          },
+          {
+            name: "Fundamentals of Multimedia",
+            id: "ITC 2342",
+          },
+        ],
+      },
+      {
+        sem: "4",
+        subs: [
+          {
+            name: "Economics and Financial Management",
+            id: "ITC 2242",
+          },
+          {
+            name: "Web Application Development",
+            id: "ITC 2223",
+          },
+          {
+            name: "Introduction to Machine Learning",
+            id: "ITC 2252",
+          },
+          {
+            name: "Advanced Data Structures and Algorithms",
+            id: "ITC 2282",
+          },
+          {
+            name: "Digital Control Systems Technology",
+            id: "ITC 2292",
+          },
+          {
+            name: "Network Systems Design",
+            id: "ITC 2302",
+          },
+          {
+            name: "Introduction to Graphic Design",
+            id: "ITC 2353",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "3",
+    sems: [
+      {
+        sem: "5",
+        subs: [
+          {
+            id: "ITC 3013",
+            name: "Sevice Oriented Architecture and Web Services",
+          },
+          {
+            id: "ITC 3023",
+            name: "Human Computer Interaction",
+          },
+          {
+            id: "ITC 3032",
+            name: "Literature Study and Presentation",
+          },
+          {
+            id: "ITC 3052",
+            name: "Project Management",
+          },
+          {
+            id: "ITC 3561",
+            name: "ICT Project",
+          },
+
+
+          {
+            id: "ITN 3043",
+            name: "Advanced Database Systems",
+            type: "n",
+          },
+          {
+            id: "ITN 3052",
+            name: "Advanced Networking",
+            type: "n",
+          },
+          {
+            id: "ITN 3132",
+            name: "Wireless Communication",
+            type: "n",
+          },
+          {
+            id: "ITS 3043",
+            name: "Advanced Database Systems",
+            type: "h",
+          },
+          {
+            id: "ITS 3551",
+            name: "Software Engineering II",
+            type: "h",
+          },
+          {
+            id: "ITM 3052",
+            name: "Video Production Techniques",
+            type: "m",
+          },
+          {
+            id: "ITM 3062",
+            name: "Audio Production Techniques",
+            type: "m",
+          },
+          {
+            id: "ITM 3232",
+            name: "Image Processing",
+            type: "m",
+          },
+        ],
+      },
+      {
+        sem: "6",
+        subs: [
+          {
+            id: "ITC 3082",
+            name: "Research Methods and Technical Writing",
+          },
+          {
+            id: "ITC 3093",
+            name: "Computer Security",
+          },
+          {
+            id: "ITC 3113",
+            name: "Software Quality Assurance",
+          },
+          {
+            id: "ITC 3122",
+            name: "Mobile Application Development",
+          },
+
+
+          {
+            id: "ITN 3102",
+            name: "System Administration",
+            type: "n",
+          },
+          {
+            id: "ITN 3152",
+            name: "Advanced Mobile Technologies",
+            type: "n",
+          },
+          {
+            id: "ITN 3162",
+            name: "Distributed Networks",
+            type: "n",
+          },
+          {
+            id: "ITS 3102",
+            name: "System Administration",
+            type: "h",
+          },
+          {
+            id: "ITS 3142",
+            name: "Software Deployment and Evolution",
+            type: "h",
+          },
+          {
+            id: "ITS 3173",
+            name: "Data Mining and Warehousing",
+            type: "h",
+          },
+          {
+            id: "ITM 3072",
+            name: "Gaming Technology",
+            type: "m",
+          },
+          {
+            id: "ITM 3132",
+            name: "3D Modeling",
+            type: "m",
+          },
+          {
+            id: "ITM 3142",
+            name: "Digital Marketing",
+            type: "m",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    year: "4",
+    sems: [
+      {
+        sem: "7",
+        subs: [
+          {
+            name: "Industrial Training/Internship",
+            id: "ITC 4156",
+          },
+          {
+            name: "Research Project",
+            id: "ITC 4166",
+          },
+        ],
+      },
+      {
+        sem: "8",
+        subs: [
+          {
+            name: "Entrepreneurship and small Bussiness Management",
+            id: "ITC 4172",
+          },
+          {
+            name: "Professional Practices in ICT",
+            id: "ITC 4182",
+          },
+          {
+            name: "Information Systems Strategy and IT Governance",
+            id: "ITC 4212",
+          },
+
+
+          {
+            name: "Advanced Topics in Networking",
+            id: "ITN 4252",
+            type: "n",
+          },
+          {
+            name: "Network Security",
+            id: "ITN 4272",
+            type: "n",
+          },
+          {
+            id: "ITS 4243",
+            name: "Microservices and Cloud Computing",
+            type: "h",
+          },
+          {
+            id: "ITS 4202",
+            name: "Emerging Technologies",
+            type: "h",
+          },
+          {
+            id: "ITM 4213",
+            name: "Immersive Technologies",
+            type: "m",
+          },
+          {
+            id: "ITM 4282",
+            name: "Animation Technology",
+            type: "m",
+          },
+        ],
+      },
+    ],
+  },
+];
